eb1e1d5f9201baf571b5ad7409675333ae170c33
patriciadunphy <dunphy.pm@gmail.com>
2020-01-31 15:06:34 +0100

    singleton

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,12 +4,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        SingleObject object = SingleObject.getInstance();
+        object.showMessage();
        Circle circle = new Circle();
        circle.move();
        circle.setArea(5.0);
-       System.out.println(circle.getArea());
+       System.out.println("Cirkelns area: "+circle.getArea());
        circle.setColour("red");
-       System.out.println(circle.getColour());
+       System.out.println("Cirkelns färg: "+circle.getColour());
+       object.showMessage();
        Triangle triangle = new Triangle();
        triangle.move();
 
